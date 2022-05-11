9333e1ae4be1e3aa38149e553ddd4e008cf16857
Sanqn <sandisvert@gmail.com>
2022-05-11 22:34:37 +0300

    les_3_4_6_fixture_autouse

---
+++ lessons/les_3_4_6_fixture_autouse.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+link = 'http://selenium1py.pythonanywhere.com/'
+
+@pytest.fixture(scope='class')
+def browser(request):
+    print("start browser for test suite..")
+    browser = webdriver.Chrome(service=Service('C:\chromedriver\chromedriver.exe'), options=webdriver.ChromeOptions())
+    def close_browser():
+        browser.quit()
+    request.addfinalizer(close_browser)
+    print('Start request')
+    return browser
+
+@pytest.fixture(autouse=True)
+def prepare_data():
+    print()
+    print("preparing some critical data for every test")
+
+class TestPage:
+    def test_guest_should_see_login_link(self, browser):
+        print("login_link1")
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        print("login_basket1")
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+
