15ebcca9bb819e643e7614a74afc5aea476f8612
Sanqn <sandisvert@gmail.com>
2022-05-13 14:45:15 +0300

    test_items

--- lessons/conftest/test_items.py
+++ lessons/conftest/test_items.py
@@ -1,11 +1,14 @@
+import time
+
 from conftest import By
+import time
 
 link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    browser.implicitly_wait(3)
+    time.sleep(30)
     browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form>.btn-add-to-basket")
 
 
