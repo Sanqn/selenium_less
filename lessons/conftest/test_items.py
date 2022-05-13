90e070b736c98d8a65e1b8e6cd138e0907a16c10
Sanqn <sandisvert@gmail.com>
2022-05-13 15:20:39 +0300

    test_items

--- lessons/conftest/test_items.py
+++ lessons/conftest/test_items.py
@@ -10,7 +10,8 @@ def test_guest_should_see_button(browser):
     browser.get(link)
     #time.sleep(30)
     button = browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form>.btn-add-to-basket")
-    assert button.text == 'Ajouter au panier' #test language France
+    #assert button.text == 'Ajouter au panier' #test language France
+    assert button.text == 'Añadir al carrito'  # test language France
 
 
 
