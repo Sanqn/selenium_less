677564b729857ac1ae71954b9f8c3b5783c27c25
Sanqn <sandisvert@gmail.com>
2022-05-13 15:15:38 +0300

    test_items

--- lessons/conftest/test_items.py
+++ lessons/conftest/test_items.py
@@ -6,10 +6,11 @@ import time
 link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_button(browser):
     browser.get(link)
-    time.sleep(30)
-    browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form>.btn-add-to-basket")
+    #time.sleep(30)
+    button = browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form>.btn-add-to-basket")
+    assert button.text == 'Ajouter au panier' #test language France
 
 
 
