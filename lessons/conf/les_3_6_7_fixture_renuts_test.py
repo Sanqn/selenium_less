32e3d2c3ab46373c9e87962ad84756555aceec4d
Sanqn <sandisvert@gmail.com>
2022-05-13 13:31:26 +0300

    les_3_6_7_fixture_renuts_test

---
+++ lessons/conf/les_3_6_7_fixture_renuts_test.py
@@ -0,0 +1,13 @@
+from conftest import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#magic_link")
+
