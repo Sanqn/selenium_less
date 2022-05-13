b20f1c37c525c0c96371572e440043c411121c1a
Sanqn <sandisvert@gmail.com>
2022-05-13 20:48:06 +0300

    test_items

--- lessons/conftest/test_items.py
+++ lessons/conftest/test_items.py
@@ -9,10 +9,7 @@ link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_button(browser):
     browser.get(link)
     #time.sleep(30)
-    assert (
-            browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket").is_displayed()
-            == True
-    ), "На странице нет кнопки добавления товара в корзину"
+    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket"), "На странице нет кнопки добавления товара в корзину"
 
 
 
